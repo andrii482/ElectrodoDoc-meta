5d1707c87f40ea30dd2e18df9bf7366bf9cc44d6
Andrii Spivak <andrii.spivak@482.solutions>
2023-12-11 10:12:11 +0200

    merge meta repository with backend repository
    Signed-off-by: Andrii Spivak <andrii.spivak@482.solutions>

---
+++ src/helpers/auth.js
@@ -0,0 +1,95 @@
+import fs from 'fs';
+import { Gateway, Wallets } from 'fabric-network';
+import jwt from 'jsonwebtoken';
+import yaml from 'js-yaml';
+import path from 'path';
+
+const issuer = '482solutions';
+
+exports.verifyToken = function verifyToken(req, authOrSecDef, token, callback) {
+  function sendError() {
+    return { code: 404, payload: { message: 'Invalid token supplied.' } };
+  }
+
+  if (token && token.indexOf('Bearer ') === 0) {
+    const tokenString = token.split(' ')[1];
+    jwt.verify(tokenString, '482solutions', (verificationError, decodedToken) => {
+      if (verificationError === null && decodedToken) {
+        const issuerMatch = decodedToken.issuer === issuer;
+        if (issuerMatch) {
+          req.auth = decodedToken;
+          return callback(null);
+        }
+        return callback(sendError());
+      }
+      return callback(sendError());
+    });
+  }
+  return callback(sendError());
+};
+
+exports.issueToken = function issueToken(username) {
+  return jwt.sign({ name: username, issuer }, '482solutions', { expiresIn: '1h' });
+};
+
+exports.validationPrivateKey = (string) => {
+  const privateKey = string.toString().trim();
+  return privateKey.startsWith('-----BEGIN PRIVATE KEY-----') && privateKey.endsWith(
+    '-----END PRIVATE KEY-----');
+};
+exports.validationCertificate = (string) => {
+  const certificate = string.toString().trim();
+  return certificate.startsWith('-----BEGIN CERTIFICATE-----') && certificate.endsWith(
+    '-----END CERTIFICATE-----');
+};
+exports.validationCSR = (string) => {
+  const csr = string.toString().trim();
+  return csr.startsWith('-----BEGIN CERTIFICATE REQUEST-----') && csr.endsWith(
+    '-----END CERTIFICATE REQUEST-----');
+};
+
+exports.getUserFromToken = async (token) => {
+  if (token && token.indexOf('Bearer ') === 0) {
+    const tokenString = token.split(' ')[1];
+    try {
+      return (await jwt.verify(tokenString, '482solutions'.toString('base64'))).data;
+    } catch (e) {
+      return null;
+    }
+  }
+  return null;
+};
+
+exports.sendTransaction = async ({ identity, transaction, network: networkOptions }) => {
+  const wallet = await Wallets.newInMemoryWallet();
+  const mixin = {
+    credentials: {
+      certificate: identity.certificate,
+      privateKey: identity.privateKey,
+    },
+    mspId: identity.mspId,
+    type: 'X.509',
+  };
+  await wallet.put(identity.label, mixin); // label == userid/login
+
+  const gateway = new Gateway();
+  try {
+    const connectionProfile = yaml.load(
+      fs.readFileSync(path.resolve(__dirname, process.env.FABRIC_CONFIG_FILE), 'utf8'),
+    );
+    const connectionOptions = {
+      identity: identity.label,
+      wallet,
+      discovery: { enabled: true, asLocalhost: true },
+    };
+    await gateway.connect(connectionProfile, connectionOptions);
+    const network = await gateway.getNetwork(networkOptions.channel);
+    const contract = network.getContract(networkOptions.chaincode) // , networkOptions.contract);
+    const issueResponse = await contract.submitTransaction(transaction.name, ...transaction.props);
+    gateway.disconnect();
+    return JSON.parse(issueResponse.toString());
+  } catch (error) {
+    gateway.disconnect();
+    return null;
+  }
+};
